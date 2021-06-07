17fcfa0d19c2286a5abee64d71ebd48d1048ba17
maryamzain123 <smaryam.zain@gmail.com>
2021-06-07 11:27:46 -0500

    commit tree 550b64aea79e9c587d99244617f9dd65f8185391

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ] ;
+
+function writeCards(names) {
+  for (let i = 1; i < names.length; i++) {
+    console.log(`'Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+ 
+}
